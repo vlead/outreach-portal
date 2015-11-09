d88789f9c1584bc7d43bd45483ff7c401d18ea91
Dinesh Padisetti <dinesh.padisetti@gmail.com>
2015-11-09 09:54:50 +0530

    Added some map url

--- src/Frontend/js/app.js
+++ src/Frontend/js/app.js
@@ -3,7 +3,9 @@
     app.controller('upcomingController', function() {
     	var upcomingList = this;
     	upcomingList.workshops = [
-    	  {date: '29/11/15', location: 'Hyderabad', coordinator: 'T.Rakesh Kumar'}];
+    	  {date: '29/11/15', location: 'Hyderabad', coordinator: 'T.Rakesh Kumar'},
+    	  {date: '29/11/15', location: 'Hyderabad', coordinator: 'T.Rakesh Kumar'}
+    	  ];
     });
      var cities1 = [
               {
