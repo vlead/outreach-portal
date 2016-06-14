12a3ab56756263201f294a8b46ffbc032fe3cbdf
ksripathi <kammari.sripathi@gmail.com>
2016-06-14 09:38:27 +0530

    headers passed for all the api requests

--- src/static/js/factories.js
+++ src/static/js/factories.js
@@ -1,18 +1,19 @@
 var service = angular.module('outreachApp.factories', []);
-service.factory('dataFactory', function($http){
+service.factory('dataFactory', function($http, $window){
+  var headers = { 'email' : $window.email, 'key' : $window.key };
     var data = {};
     data.fetch = function(url){
         return $http.get(url);
-    }
+    };
     data.post = function(url, data){
-        return $http.post(url, data);
-    }
+      return $http.post(url, data, {headers : headers });
+    };
     data.put = function(url, data){
-        return $http.put(url, data);
-    }
+      return $http.put(url, data, { headers : headers });
+    };
     data.del = function(url){
-        return $http.delete(url);
-    }
+      return $http.delete(url, { headers : headers });
+    };
     
     
     return data;
